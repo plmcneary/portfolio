75daf73e8920a4c81514b544ea3fe7d216e08adb
Peter McNeary <plmcneary@icloud.com>
2015-11-07 16:36:40 -0500

    Tweaking scroll, section height

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,10 @@
 (function () {
+  var aboutHeight = $('#about').css('min-height').match(/[0-9]+/)[0] - 60 + "px";
+  $('#about').css({'min-height': aboutHeight});
+
+  var portfolioHeight = $('#portfolio').css('min-height').match(/[0-9]+/)[0] - 60 + "px";
+  $('#portfolio').css({'min-height': portfolioHeight});
+
   $('.home').on('click', function() {
     $('html, body').animate({
       scrollTop: $("#splash").offset().top
@@ -7,19 +13,19 @@
 
   $('.about').on('click', function() {
     $('html, body').animate({
-        scrollTop: $("#about").offset().top - 60
+        scrollTop: $("#about").offset().top - 59
     }, 'slow');
   })
 
   $('.portfolio').on('click', function() {
     $('html, body').animate({
-        scrollTop: $("#portfolio").offset().top - 60
+        scrollTop: $("#portfolio").offset().top - 59
     }, 'slow');
   })
 
   $('.contact').on('click', function() {
     $('html, body').animate({
-        scrollTop: $("#contact").offset().top - 60
+        scrollTop: $("#contact").offset().top - 59
     }, 'slow');
   })
 })();
